8efc83c01903976c38392ee8cfad120cc9abef36
wangari <wangarigichukis@gmail.com>
2023-04-20 07:28:13 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed by a new line
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
