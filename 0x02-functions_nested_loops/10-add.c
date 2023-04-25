27cb09f48228513da5af080e60500684d3d28a4c
wangari <wangarigichukis@gmail.com>
2023-04-25 08:40:53 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add two values
+ * @n1: The first number to be executed
+ * @n2: The second number to be executed
+ *
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
