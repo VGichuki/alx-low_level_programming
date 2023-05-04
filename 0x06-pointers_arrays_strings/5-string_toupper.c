98b2f1f51c3d8707ee55935e551cef3694b23df8
wangari <wangarigichukis@gmail.com>
2023-05-04 07:30:58 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters
+ * @str: a string to be changed
+ *
+ * Return: A pointer to be changed string
+ */
+
+char *string_toupper(char *str)
+{
+int index = 0;
+while (str[index])
+{
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
+index++;
+}
+return (str);
+}
