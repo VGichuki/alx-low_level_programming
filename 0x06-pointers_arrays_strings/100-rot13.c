dbb1cc268c9d6dcf86d26408900f3ee74b51de09
wangari <wangarigichukis@gmail.com>
2023-05-04 09:42:38 -0700

    corrections

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,7 +16,7 @@ char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 32; j++)
+for (j = 0; j < 52; j++)
 {
 if (s[i] == data1[j])
 {
