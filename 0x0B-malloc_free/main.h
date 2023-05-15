4971cac0b8151ce37d010d97c86e9c50ae128a9b
wangari <wangarigichukis@gmail.com>
2023-05-15 13:04:12 -0700

    add a main file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+void simple_print_buffer(char *buffer, unsigned int size);
+int main(void);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void print_grid(int **grid, int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+int main(int ac, char *av[]);
+char **strtow(char *str);
+void print_tab(char **tab);
+int _putchar(char c);
+
+#endif /* MAIN_H */
