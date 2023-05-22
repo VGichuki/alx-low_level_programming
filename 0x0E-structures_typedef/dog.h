a39c329eed012cbcd9a86b70a24fe2a83c633f4c
wangari <wangarigichukis@gmail.com>
2023-05-22 05:37:53 -0700

    dog,h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG
+#define DOG
 
 /**
  * struct dog -  dog attributes
@@ -17,14 +17,13 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - typedef for dog structure
- */
-
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 #endif
