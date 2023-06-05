d765b82151f9381d75c6dff354191562a4799598
wangari <wangarigichukis@gmail.com>
2023-06-05 14:42:42 -0700

    7-get_nodeint.c file)

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,14 +10,16 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *temp = head;
+	unsigned int i;
 
-	while (temp && i < index)
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
 	{
-		temp = temp->next;
-		i++;
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
 
-	return (temp ? temp : NULL);
+	return (head);
 }
